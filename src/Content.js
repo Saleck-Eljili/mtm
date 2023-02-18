1f895e1cb715572e13d1be730b833e3afe5cce69
Saleck-Eljili <salek.eljili@gmail.com>
2023-02-18 22:02:18 +0100

    mtm test

---
+++ src/Content.js
@@ -0,0 +1,216 @@
+// import images
+import Hero_person from "./assets/images/Hero/person.png";
+
+import figma from "./assets/images/Skills/figma.png";
+import sketch from "./assets/images/Skills/sketch.png";
+import ps from "./assets/images/Skills/ps.png";
+import reactjs from "./assets/images/Skills/react.png";
+import nodejs from "./assets/images/Skills/node.png";
+import python from "./assets/images/Skills/python.png";
+
+import services_logo1 from "./assets/images/Services/istockphoto-1270573960-170667a-removebg-preview.png";
+import services_logo2 from "./assets/images/Services/icône-de-gestion-des-projets-dans-le-style-plat-symbole-projet-pour-votre-conception-site-web-logo-appli-ui-vecteur-illustration-144607341-removebg-preview.png";
+import services_logo3 from "./assets/images/Services/old-truck-logo-vector-black-white-illustration-front-view-transport-218026424-removebg-preview.png";
+
+import project1 from "./assets/images/projects/img1.png";
+import project2 from "./assets/images/projects/img2.png";
+import project3 from "./assets/images/projects/img3.png";
+import person_project from "./assets/images/projects/person.png";
+
+import avatar1 from "./assets/images/Testimonials/avatar1.png";
+import avatar2 from "./assets/images/Testimonials/avatar2.png";
+import avatar3 from "./assets/images/Testimonials/avatar3.png";
+import avatar4 from "./assets/images/Testimonials/avatar4.png";
+
+import Hireme_person from "./assets/images/Hireme/person.png";
+import Hireme_person2 from "./assets/images/Hireme/person2.png";
+
+// import icons from react-icons
+import { GrMail } from "react-icons/gr";
+import { MdArrowForward, MdCall, MdMessage } from "react-icons/md";
+import { BsInstagram } from "react-icons/bs";
+import { TbSmartHome } from "react-icons/tb";
+import { BiUser } from "react-icons/bi";
+import { RiServiceLine, RiProjectorLine } from "react-icons/ri";
+import { MdOutlinePermContactCalendar } from "react-icons/md";
+
+export const content = {
+  nav: [
+    {
+      link: "#home",
+      icon: TbSmartHome,
+    },
+    /* {
+      link: "#skills",
+      icon: BiUser,
+    }, */
+    {
+      link: "#services",
+      icon: RiServiceLine,
+    },
+    /* {
+      link: "#projects",
+      icon: RiProjectorLine,
+    }, */
+    {
+      link: "#contact",
+      icon: MdOutlinePermContactCalendar,
+    },
+  ],
+  hero: {
+    title: "MTM MULTI SERVICES SARL",
+    firstName: "MTM MULTI",
+    LastName: "SERVICES SARL",
+    btnText: "Contactez Nous",
+    image: Hero_person,
+    hero_content: [
+      {
+        count: "",
+        text: "MTM souhaite contribuer à la protection de l’environnement en Mauritanie.",
+      },
+      {
+        count: "",
+        text: "",
+      },
+    ],
+  },
+  skills: {
+    title: "Skills",
+    subtitle: "MY TOP SKILLS",
+    skills_content: [
+      {
+        name: "Figma",
+        para: "Lorem ipsum text  dummy",
+        logo: figma,
+      },
+      {
+        name: "Node js",
+        para: "Lorem ipsum text  dummy",
+        logo: nodejs,
+      },
+      {
+        name: "Adobe Photoshop",
+        para: "Lorem ipsum text  dummy",
+        logo: ps,
+      },
+      {
+        name: "React js",
+        para: "Lorem ipsum text  dummy",
+        logo: reactjs,
+      },
+      {
+        name: "Sketch",
+        para: "Lorem ipsum text  dummy",
+        logo: sketch,
+      },
+      {
+        name: "Python",
+        para: "Lorem ipsum text  dummy",
+        logo: python,
+      },
+    ],
+    icon: MdArrowForward,
+  },
+  services: {
+    title: "Services",
+    subtitle: "CE QUE NOUS OFFRONS",
+    service_content: [
+      {
+        title: "Services pétrolier",
+        para: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
+        logo: services_logo1,
+      },
+      {
+        title: "Études des projets",
+        para: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
+        logo: services_logo2,
+      },
+      {
+        title: "Support des compagnies en matière de logistique",
+        para: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
+        logo: services_logo3,
+      },
+    ],
+  },
+  Projects: {
+    title: "Projects",
+    subtitle: "MY CREATION",
+    image: person_project,
+    project_content: [
+      {
+        title: "Gym Website",
+        image: project1,
+      },
+      {
+        title: "Social Media web",
+        image: project2,
+      },
+      {
+        title: "Creative Website",
+        image: project3,
+      },
+    ],
+  },
+  Testimonials: {
+    title: "Testimonials",
+    subtitle: "MY CLIENT REVIEWS",
+    testimonials_content: [
+      {
+        review:
+          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+        img: avatar1,
+        name: "JOHN DOE",
+      },
+      {
+        review:
+          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+        img: avatar2,
+        name: "Tom Alex",
+      },
+      {
+        review:
+          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+        img: avatar3,
+        name: "Johnny",
+      },
+      {
+        review:
+          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+        img: avatar4,
+        name: "ROBBIN",
+      },
+    ],
+  },
+  Hireme: {
+    title: "Hire Me",
+    subtitle: "FOR YOUR PROJECTS",
+    image1: Hireme_person,
+    image2: Hireme_person2,
+    para: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
+    btnText: "Hire Me",
+  },
+  Contact: {
+    title: "Contactez Nous",
+    subtitle: "ENTRER EN CONTACT",
+    social_media: [
+      {
+        text: "test@gmail.com",
+        icon: GrMail,
+        link: "mailto:test@gmail.com",
+      },
+      {
+        text: "+213 669130516",
+        icon: MdCall,
+        link: "https://wa.me/213669130516",
+      },
+      {
+        text: "+213 669130516",
+        icon: MdMessage,
+        link: "https://wa.me/213669130516",
+      },
+    ],
+  },
+  Footer: {
+    text: "All © Copy Right Reserved 2023",
+  },
+};
